fa71c6114fe740b551de10d2ae42ae1100cd3be8
Devlin Glasman <devlin.glasman@gmail.com>
2018-05-26 10:04:14 +0100

    Refactors Minimax.

--- src/main/java/Core/Minimax.java
+++ src/main/java/Core/Minimax.java
@@ -5,43 +5,42 @@ import java.util.ArrayList;
 public class Minimax {
 
     private Mark minimaxPlayerMark;
+    private Grid firstGrid;
 
-    public Minimax(Mark minimaxPlayerMark) {
+    public Minimax(Mark minimaxPlayerMark, Grid firstGrid) {
         this.minimaxPlayerMark = minimaxPlayerMark;
+        this.firstGrid = firstGrid;
     }
 
-    public int returnScoreForTerminalGameState(Grid grid) {
-        if (grid.winningLineExistsInGrid()) {
-            if (grid.reportWinningMark() == minimaxPlayerMark) return 1;
-            else return -1;
-        } else return 0;
-    }
-
-    public int findGameStateScore(Grid grid, Mark optimisingPlayerMark) {
+    public int findSquareChoice(Grid grid, Mark optimisingPlayerMark) {
 
         ArrayList<Integer> emptyGridSquares = grid.emptySquareIndices();
         ArrayList<Integer> scores = new ArrayList<>();
 
         for (Integer emptySquare : emptyGridSquares) {
-            Grid gridClone = grid.duplicate();
-            gridClone.markSquare(emptySquare, optimisingPlayerMark);
-            if (gridClone.isGameOver()) {
-                Integer scoreForEmulate = returnScoreForTerminalGameState(gridClone);
+            Grid emulatedGrid = emulateGrid(grid, emptySquare, optimisingPlayerMark);
+            if (emulatedGrid.isGameOver()) {
+                Integer scoreForEmulate = returnScoreForTerminalGameState(emulatedGrid);
                 scores.add(scoreForEmulate);
             } else {
-                Mark temporaryOptimisingPlayer;
-                if (optimisingPlayerMark == minimaxPlayerMark) temporaryOptimisingPlayer = Mark.playerOneMark;
-                else temporaryOptimisingPlayer = minimaxPlayerMark;
-                Integer gameScore = findGameStateScore(gridClone, temporaryOptimisingPlayer);
+                Mark oppositeOptimiserMark = makeOppositeOptimisingMark(optimisingPlayerMark);
+                Integer gameScore = findSquareChoice(emulatedGrid, oppositeOptimiserMark);
                 scores.add(gameScore);
             }
         }
-        if (optimisingPlayerMark == minimaxPlayerMark) {
-            Integer maxIndex = findMaxIndex(scores);
-            return scores.get(maxIndex);
+
+        if (grid == firstGrid) {
+            Integer maxIndex;
+            maxIndex = findMaxIndex(scores);
+            return emptyGridSquares.get(maxIndex);
         } else {
-            Integer minIndex = findMinIndex(scores);
-            return scores.get(minIndex);
+            if (optimisingPlayerMark == minimaxPlayerMark) {
+                Integer maxIndex = findMaxIndex(scores);
+                return scores.get(maxIndex);
+            } else {
+                Integer minIndex = findMinIndex(scores);
+                return scores.get(minIndex);
+            }
         }
     }
 
@@ -67,5 +66,25 @@ public class Minimax {
             }
         }
         return minIndex;
+    }
+
+    private int returnScoreForTerminalGameState(Grid grid) {
+        if (grid.winningLineExistsInGrid()) {
+            if (grid.reportWinningMark() == minimaxPlayerMark) return 1;
+            else return -1;
+        } else return 0;
+    }
+
+    private Grid emulateGrid(Grid grid, Integer emptySquare, Mark mark) {
+        Grid gridClone = grid.duplicate();
+        gridClone.markSquare(emptySquare, mark);
+        return gridClone;
+    }
+
+    private Mark makeOppositeOptimisingMark(Mark currentOptimisingPlayerMark) {
+        Mark oppositeOptimisingMark;
+        if (currentOptimisingPlayerMark == minimaxPlayerMark) oppositeOptimisingMark = Mark.playerOneMark;
+        else oppositeOptimisingMark = minimaxPlayerMark;
+        return oppositeOptimisingMark;
     }
 }
--- src/main/java/Core/Players/PlayerComputer.java
+++ src/main/java/Core/Players/PlayerComputer.java
@@ -4,35 +4,16 @@ import Core.Grid;
 import Core.Mark;
 import Core.Minimax;
 
-import java.util.ArrayList;
-
 public class PlayerComputer extends Player {
-
-    private Minimax minimax;
 
     public PlayerComputer(String name, Mark mark) {
         super(name, mark);
-        minimax = new Minimax(getMark());
     }
 
     @Override
     public int getInput(Grid grid) {
-        ArrayList<Integer> emptyGridSquares = grid.emptySquareIndices();
-        ArrayList<Integer> scores = new ArrayList<>();
-
-        for (Integer emptySquare : emptyGridSquares) {
-            Grid gridClone = grid.duplicate();
-            gridClone.markSquare(emptySquare, getMark());
-            if (gridClone.isGameOver()) {
-                Integer scoreForEmulate = minimax.returnScoreForTerminalGameState(gridClone);
-                scores.add(scoreForEmulate);
-            } else {
-                Integer gameScore = minimax.findGameStateScore(gridClone, Mark.playerOneMark);
-                scores.add(gameScore);
-            }
-        }
-        Integer maxIndex = minimax.findMaxIndex(scores);
-        return emptyGridSquares.get(maxIndex);
+       Minimax minimax = new Minimax(getMark(), grid);
+        return minimax.findSquareChoice(grid, getMark());
     }
 
     @Override
