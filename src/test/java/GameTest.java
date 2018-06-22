9546776019d7b5194ceda597a3e5bc3a8f256ce0
Devlin Glasman <devlin.glasman@gmail.com>
2018-06-22 13:32:25 +0100

    Refactors gameRun test.

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -8,6 +8,9 @@ import Core.Players.PlayerHuman;
 import Core.UI;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
@@ -19,11 +22,16 @@ public class GameTest {
         Player playerOne = new PlayerComputer("Player One", Mark.playerOneMark, Mark.playerTwoMark);
         Player playerTwo = new PlayerComputer("Player Two", Mark.playerTwoMark, Mark.playerOneMark);
         Game game = new Game(grid, playerOne, playerTwo, ui);
+        ArrayList<Mark> squaresResult = new ArrayList<>(
+                asList(Mark.playerOneMark, Mark.playerOneMark, Mark.playerOneMark,
+                        Mark.unmarkedSquare, Mark.unmarkedSquare, Mark.unmarkedSquare,
+                        Mark.unmarkedSquare, Mark.unmarkedSquare, Mark.unmarkedSquare));
 
         grid.markSquare(0, Mark.playerOneMark);
         grid.markSquare(1, Mark.playerOneMark);
         game.runGame();
 
+        assertEquals(squaresResult, grid.getSquares());
         assertEquals(playerOne, game.getActivePlayer());
     }
 }
