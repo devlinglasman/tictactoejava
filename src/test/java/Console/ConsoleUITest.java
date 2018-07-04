923d4bdc9481bcd9bd91380ac9419f302889d596
Devlin Glasman <devlin.glasman@gmail.com>
2018-07-04 14:46:17 +0100

    Refactors grid display tests.

--- src/test/java/Console/ConsoleUITest.java
+++ src/test/java/Console/ConsoleUITest.java
@@ -23,7 +23,7 @@ public class ConsoleUITest {
     }
 
     @Test
-    public void displayGrid() {
+    public void displayGrid_allUnmarkedSquares() {
         IOHelper ioHelper = new IOHelper("");
         ConsoleUI consoleUI = new ConsoleUI(ioHelper.in, ioHelper.print, 0);
 
@@ -33,15 +33,17 @@ public class ConsoleUITest {
         }
         consoleUI.displayGrid(squares);
 
-        Assert.assertEquals("\n" + (ANSI_BRIGHTYELLOW + "[1]" + ANSI_RESET) + (ANSI_BRIGHTRED + "[2]" + ANSI_RESET) +
-                (ANSI_BRIGHTPURPLE + "[3]" + ANSI_RESET) + "\n" + (ANSI_BRIGHTRED + "[4]" + ANSI_RESET) +
-                (ANSI_BRIGHTPURPLE + "[5]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[6]" + ANSI_RESET) + "\n" +
-                (ANSI_BRIGHTPURPLE + "[7]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[8]" + ANSI_RESET) +
-                (ANSI_BRIGHTGREEN + "[9]" + ANSI_RESET + "\n"), ioHelper.output());
+        Assert.assertEquals("\n" + (ANSI_BRIGHTYELLOW + "[1]" + ANSI_RESET) +
+                (ANSI_BRIGHTRED + "[2]" + ANSI_RESET) + (ANSI_BRIGHTPURPLE + "[3]" +
+                ANSI_RESET) + "\n" + (ANSI_BRIGHTRED + "[4]" + ANSI_RESET) +
+                (ANSI_BRIGHTPURPLE + "[5]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[6]" +
+                ANSI_RESET) + "\n" + (ANSI_BRIGHTPURPLE + "[7]" + ANSI_RESET) +
+                (ANSI_BRIGHTBLUE + "[8]" + ANSI_RESET) + (ANSI_BRIGHTGREEN + "[9]"
+                + ANSI_RESET + "\n"), ioHelper.output());
     }
 
     @Test
-    public void displayGrid2() {
+    public void displayGrid_firstSquarePlayerOneMark() {
         IOHelper ioHelper = new IOHelper("");
         ConsoleUI consoleUI = new ConsoleUI(ioHelper.in, ioHelper.print, 0);
 
@@ -53,17 +55,42 @@ public class ConsoleUITest {
 
         consoleUI.displayGrid(squares);
 
-        Assert.assertEquals("\n" + (ANSI_BRIGHTBLACK + "[X]" + ANSI_RESET) + (ANSI_BRIGHTRED + "[2]" + ANSI_RESET) +
-                (ANSI_BRIGHTPURPLE + "[3]" + ANSI_RESET) + "\n" + (ANSI_BRIGHTRED + "[4]" + ANSI_RESET) +
-                (ANSI_BRIGHTPURPLE + "[5]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[6]" + ANSI_RESET) + "\n" +
-                (ANSI_BRIGHTPURPLE + "[7]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[8]" + ANSI_RESET) +
-                (ANSI_BRIGHTGREEN + "[9]" + ANSI_RESET + "\n"), ioHelper.output());
+        Assert.assertEquals("\n" + (ANSI_BRIGHTBLACK + "[" +
+                Mark.PLAYER_ONE.getStringRepresentation() + "]" + ANSI_RESET)
+                + (ANSI_BRIGHTRED + "[2]" + ANSI_RESET) + (ANSI_BRIGHTPURPLE +
+                "[3]" + ANSI_RESET) + "\n" + (ANSI_BRIGHTRED + "[4]" + ANSI_RESET) +
+                (ANSI_BRIGHTPURPLE + "[5]" + ANSI_RESET) + (ANSI_BRIGHTBLUE +
+                "[6]" + ANSI_RESET) + "\n" + (ANSI_BRIGHTPURPLE + "[7]" + ANSI_RESET)
+                + (ANSI_BRIGHTBLUE + "[8]" + ANSI_RESET) + (ANSI_BRIGHTGREEN + "[9]"
+                + ANSI_RESET + "\n"), ioHelper.output());
     }
 
     @Test
-    public void clearScreen() {
+    public void displayGrid_secondSquarePlayerTwoMark() {
         IOHelper ioHelper = new IOHelper("");
-        ConsoleUI consoleUI = new ConsoleUI(ioHelper.in, ioHelper.print, 1);
+        ConsoleUI consoleUI = new ConsoleUI(ioHelper.in, ioHelper.print, 0);
+
+        List<Mark> squares = new ArrayList<>();
+        squares.add(Mark.EMPTY);
+        squares.add(Mark.PLAYER_TWO);
+        for (int i = 0; i < 7; i++) {
+            squares.add(Mark.EMPTY);
+        }
+
+        consoleUI.displayGrid(squares);
+
+        Assert.assertEquals("\n" + (ANSI_BRIGHTYELLOW + "[1]" + ANSI_RESET)
+                + (ANSI_BRIGHTWHITE + "[" + Mark.PLAYER_TWO.getStringRepresentation()
+                + "]" + ANSI_RESET) + (ANSI_BRIGHTPURPLE + "[3]" + ANSI_RESET) + "\n"
+                + (ANSI_BRIGHTRED + "[4]" + ANSI_RESET) + (ANSI_BRIGHTPURPLE + "[5]"
+                + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[6]" + ANSI_RESET) + "\n" +
+                (ANSI_BRIGHTPURPLE + "[7]" + ANSI_RESET) + (ANSI_BRIGHTBLUE + "[8]" + ANSI_RESET) +
+                (ANSI_BRIGHTGREEN + "[9]" + ANSI_RESET + "\n"), ioHelper.output());
+    }
+    @Test
+    public void clearScreen_onMac() {
+        IOHelper ioHelper = new IOHelper("");
+        ConsoleUI consoleUI = new ConsoleUI(ioHelper.in, ioHelper.print, 0);
 
         consoleUI.clearScreen();
 
